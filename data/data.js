10f6b328ed091caef7d6b59080cb0c117985e039
Calpay6807 <alpayc3@gmail.com>
2023-11-14 13:23:30 +0300

    native

---
+++ data/data.js
@@ -0,0 +1,7 @@
+const choices = [
+  {name: 'Taş', image: require('../assets/rock-paper/tas.png')},
+  {name: 'Kağıt', image: require('../assets/rock-paper/kagit.png')},
+  {name: 'Makas', image: require('../assets/rock-paper/makas.png')},
+];
+
+export default choices;
